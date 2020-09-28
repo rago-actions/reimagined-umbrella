84df669afe94ea564087d4e17ae7a0b84a3594a3
Ramesh Godishela <49201509+rago-actions@users.noreply.github.com>
2020-09-28 22:44:34 +0530

    Update main.tf

--- s1/helloworld_envvars/main.tf
+++ s1/helloworld_envvars/main.tf
@@ -15,4 +15,7 @@ provider "aws" {
 resource "aws_instance" "helloworld" {
   ami           = "ami-0cda377a1b884a1bc"
   instance_type = "t2.micro"
+  tags          = {
+    Name = "helloworld_envvars"
+  }
 }
